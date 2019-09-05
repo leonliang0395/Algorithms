5abbf5b68ad836a0ce6f4f4e503e6554035918ce
Leon Liang <lelian@microsoft.com>
2019-09-05 15:08:02 -0700

    adding subarray sum problem

--- src/Arrays.cpp
+++ src/Arrays.cpp
@@ -49,6 +49,46 @@ int maxTwoTrades(const vector<int> &arr) {
     return maxTwoTrades;
 }
 
+int subarraySum(vector<int>& nums, int sum) {
+    // STL map to store number of subarrays 
+    // starting from index zero having  
+    // particular value of sum. 
+    unordered_map<int, int> prevSum; 
+
+    int res = 0; 
+
+    // Sum of elements so far. 
+    int currsum = 0; 
+
+    for (int i = 0; i < nums.size(); i++) { 
+
+        // Add current element to sum so far. 
+        currsum += nums[i]; 
+
+        // If currsum is equal to desired sum, 
+        // then a new subarray is found. So 
+        // increase count of subarrays. 
+        if (currsum == sum)  
+            res++;         
+
+        // currsum exceeds given sum by currsum  
+        //  - sum. Find number of subarrays having  
+        // this sum and exclude those subarrays 
+        // from currsum by increasing count by  
+        // same amount. 
+        if (prevSum.find(currsum - sum) !=  
+                                prevSum.end())  
+            res += (prevSum[currsum - sum]); 
+        
+
+        // Add currsum value to count of  
+        // different values of sum. 
+        prevSum[currsum]++; 
+    } 
+
+    return res; 
+}
+
 pair<pair<int, int>, int> maxContiguousSubarray(const vector<int> &arr) {
     int maxSoFar = arr[0];
     int maxNow = arr[0];
